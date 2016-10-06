292bd1c78d55b5359a0bff39fd4dcb942e5e0410
Cesar Guerrero <cesar.guerrero@searshc.com>
2016-10-05 23:30:01 -0500

    pushing to see online

---
+++ js/main.js
@@ -0,0 +1,34 @@
+(function (){
+	"use strict";
+	window.HELP_IMPROVE_VIDEOJS = false;
+	videojs.options.flash.swf = "../bower_components/video.js/dist/video-js.swf";
+	var videos = [{
+		src: 'http://bad-videos.dev.zype.com/good-video1/ArduinoTheDocumentary.mp4',
+		type: 'video/mp4'
+	}, 
+	{
+		src: 'http://bad-videos.dev.zype.com/good-video2/sintel-2048-stereo.mp4',
+		type: 'video/mp4'
+	},
+	{
+		src: 'http://bad-videos.dev.zype.com/good-video3/bipbopall.m3u8',
+		type: "application/x-mpegURL"
+	}];
+
+	var options = {
+		hls: {
+			withCredentials: true
+		}
+	};
+
+	var player = videojs('good-videos', {flash: options, html5: options});
+
+	//Default Video
+	player.src({"type": videos[2].type, "src":videos[2].src});
+
+	$(".change-video").click(function() {
+		var $this = $(this);
+	});
+
+
+})();
